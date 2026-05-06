98eabbea8ad9cba641acc1d1ca80fda9551e71ec
AthirahWahyu <athirahsasongko07@gmail.com>
2026-05-06 10:26:29 +0700

    Praktikum 2 | Modifikasi Elemen pada Single Linked List

--- Pertemuan12/SLLMain03.java
+++ Pertemuan12/SLLMain03.java
@@ -9,16 +9,35 @@ public class SLLMain03 {
         Mahasiswa03 mhs3 = new Mahasiswa03("23212201",  "Bimon", "2B", 3.8);
         Mahasiswa03 mhs4 = new Mahasiswa03("21212203",  "Dirga", "4D", 3.6);
 
-        sll.print(); // kondisi kosong
-
         sll.addFirst(mhs4); // menambah Dirga
-        sll.print();
-
         sll.addLast(mhs1); // menambah Alvaro di akhir
-        sll.print();
-
         sll.insertAfter("Dirga", mhs3); // menambah Bimon setelah Dirga
         sll.insertAt(1, mhs2); // menambah Cintia di index 1 (sebelum Bimon
+        
+        System.out.println("Kondisi Awal:");
+        sll.print();
+
+        // Mengakses data pada indeks tertentu
+        System.out.println("data index 1: ");
+        sll.getData(1);
+
+        // Mencari indeks berdasarkan Nama (key)
+        System.out.println("data mahasiswa an Bimon berada pada index: " + sll.indexOf("Bimon"));
+        System.out.println();
+
+        // Menghapus data paling depan
+        sll.removeFirst();
+
+        // Menghapus data paling belakang
+        sll.removeLast();
+
+        // Cetak hasil setelah removeFirst dan removeLast
+        sll.print();
+
+        // Menghapus data pada indeks tertentu (indeks 0)
+        sll.removeAt(0);
+
+        // Mencetak hasil akhir
         sll.print();
     }
 }
